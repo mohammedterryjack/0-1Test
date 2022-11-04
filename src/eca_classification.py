6b3e51d903a6380e8c0fb7d87c1c513ca4c9e908
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-11-04 10:03:41 +0000

    Update eca_classification.py

--- src/eca_classification.py
+++ src/eca_classification.py
@@ -6,12 +6,13 @@ from src.utils import observables, equivalent_eca_rules
 
 def classify_eca_rules(
     n_angles:int, rules:List[int], lattice_width:int,
-    n_iterations:int, initial_condition:int    
+    n_iterations:int, initial_condition:int, ignore_initial_transient:int   
 ) -> Generator[Tuple[int,float],None,None]:
     for rule in rules:
         phi = observables(max_time=n_iterations,eca_rule=rule,dimension=lattice_width, ic=initial_condition)
-        K = O1TestForChaos.test_for_chaos(observables=phi,n_angles=n_angles)
+        K = O1TestForChaos.test_for_chaos(observables=phi[ignore_initial_transient:],n_angles=n_angles)
         yield (rule, K)
+
 
         
 if __name__ == "__main__":
@@ -20,6 +21,7 @@ if __name__ == "__main__":
         rules=list(equivalent_eca_rules()),
         lattice_width=637,
         n_iterations=1000,
+        ignore_initial_transient=10,
         initial_condition=157772325949337114751228396077633682068852574612949832419054851897545068335997284508864138640431125523412626177597802569043207131753005708372421948440114274698313298513533339618679949586869846
     ))
     with open("results/classification.json","w") as results_file:
