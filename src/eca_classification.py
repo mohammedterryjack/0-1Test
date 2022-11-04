7992c1a91af7ee417a4837acde5a3a0f4d9ae6a8
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-11-04 11:21:27 +0000

    Update eca_classification.py

--- src/eca_classification.py
+++ src/eca_classification.py
@@ -1,5 +1,4 @@
 from typing import Generator, Tuple
-from json import dumps 
 
 from src.utils import observables, equivalent_eca_rules
 
@@ -24,5 +23,3 @@ if __name__ == "__main__":
         ignore_initial_transient=10,
         initial_condition=157772325949337114751228396077633682068852574612949832419054851897545068335997284508864138640431125523412626177597802569043207131753005708372421948440114274698313298513533339618679949586869846
     ))
-    with open("results/classification.json","w") as results_file:
-        results_file.write(dumps(results,indent=2))
