ef1ba24aa2e06ed833957fa97a84a7ddf3e66456
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-11-04 09:13:13 +0000

    Update eca_classification.py

--- src/eca_classification.py
+++ src/eca_classification.py
@@ -1,27 +1,27 @@
-from typing import Generator, Dict
+from typing import Generator, Dict, List
 from json import dumps 
 
-from src.utils import observables
+from src.utils import observables, equivalent_eca_rules
 
 
 def classify_eca_rules(
-    n_angles:int, n_rules:int, lattice_width:int,
+    n_angles:int, rules:List[int], lattice_width:int,
     n_iterations:int, initial_condition:int    
 ) -> Dict[int,float]:
     measurements = _chaos_test_on_eca_rules(
         n_angles=n_angles,
-        n_rules=n_rules,
+        rules=rules,
         lattice_width=lattice_width,
         n_iterations=n_iterations,
         initial_condition=initial_condition
     )
-    return dict(zip(range(n_rules),measurements))
+    return dict(zip(rules,measurements))
 
 def _chaos_test_on_eca_rules(
-    n_angles:int, n_rules:int, lattice_width:int,
+    n_angles:int, rules:List[int], lattice_width:int,
     n_iterations:int, initial_condition:int
 ) -> Generator[float,None,None]:
-    for rule in range(n_rules):
+    for rule in rules:
         phi = observables(max_time=n_iterations,eca_rule=rule,dimension=lattice_width, ic=initial_condition)
         yield O1TestForChaos.test_for_chaos(observables=phi,n_angles=n_angles)
         
@@ -31,7 +31,7 @@ def _chaos_test_on_eca_rules(
 if __name__ == "__main__":
     results = classify_eca_rules(
         n_angles=10,
-        n_rules=256,
+        rules=list(equivalent_eca_rules()),
         lattice_width=637,
         n_iterations=318,
         initial_condition=157772325949337114751228396077633682068852574612949832419054851897545068335997284508864138640431125523412626177597802569043207131753005708372421948440114274698313298513533339618679949586869846
