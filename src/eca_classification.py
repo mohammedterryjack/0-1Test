852c1f69039967deb579c84f74e2ea8c51dbaf54
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2022-11-04 09:16:22 +0000

    Update eca_classification.py

--- src/eca_classification.py
+++ src/eca_classification.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         n_angles=10,
         rules=list(equivalent_eca_rules()),
         lattice_width=637,
-        n_iterations=318,
+        n_iterations=1000,
         initial_condition=157772325949337114751228396077633682068852574612949832419054851897545068335997284508864138640431125523412626177597802569043207131753005708372421948440114274698313298513533339618679949586869846
     )
     with open("results/classification.json","w") as results_file:
