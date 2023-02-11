c0bd08ac6000173b32cbab0543e7b115275b211b
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2023-02-11 00:54:28 +0000

    Update eca_classification.py

--- experiments/eca_classification.py
+++ experiments/eca_classification.py
@@ -1,23 +1,6 @@
-from typing import Generator, Tuple
-from experiments.utils import observables, equivalent_eca_rules, parallel_map
+from typing import Tuple
+from experiments.utils import observables
 from src.o1_test_for_chaos import O1TestForChaos
-
-def classify_eca_rules(
-    n_angles:int, lattice_width:int,
-    n_iterations:int, initial_condition:int, 
-    ignore_initial_transient:int   
-) -> Generator[Tuple[int,float],None,None]:
-    return parallel_map(
-        lambda rule: classify_eca_rule(
-            rule=rule,
-            n_angles=n_angles, 
-            lattice_width=lattice_width,
-            n_iterations=n_iterations, 
-            initial_condition=initial_condition, 
-            ignore_initial_transient=ignore_initial_transient   
-        ),
-        equivalent_eca_rules()
-    )
 
 def classify_eca_rule(
     rule:int,
